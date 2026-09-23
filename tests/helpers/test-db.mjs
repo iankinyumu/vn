b99146e24e8ee640f3626134d4fd78072379145e
ian kinyumu <kinyumuian9@gmail.com>
2026-09-23 06:18:43 +0300

    feat: expose account-aware engine configuration

--- tests/helpers/test-db.mjs
+++ tests/helpers/test-db.mjs
@@ -82,7 +82,8 @@ export async function createTestDatabase() {
         '20260920440000_engine_customer_rpc_hardening.sql',
         '20260920450000_engine_real_gate_hardening.sql',
         '20260920460000_engine_epoch_uuid_fix.sql',
-        '20260920470000_engine_admin_rpc_surface.sql'
+        '20260920470000_engine_admin_rpc_surface.sql',
+        '20260920480000_engine_config_rpc.sql'
     ];
 
     for (const name of migrations) {
